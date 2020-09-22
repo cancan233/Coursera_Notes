ec1d56d6c4e7bbcd9dfe1f9495ef6e4db4089d2a
Cancan <cancan_huang@brown.edu>
2020-09-22 20:56:00 +0000

    add week3 part 4 solution

--- Algorithmic Toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
@@ -4,6 +4,8 @@ import sys
 
 def max_dot_product(a, b):
     #write your code here
+    a = sorted(a)
+    b = sorted(b)
     res = 0
     for i in range(len(a)):
         res += a[i] * b[i]
@@ -12,6 +14,7 @@ def max_dot_product(a, b):
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
+    # data = [3, 1, 3, -5, -2, 4, 1]
     n = data[0]
     a = data[1:(n + 1)]
     b = data[(n + 1):]
