3a480bbfbb96345223a9e1fc42ecba189035bdb8
cancan233 <cancan_huang@brown.edu>
2020-09-22 23:17:03 -0400

    add week3 solutions for 4-7

--- Algorithmic Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -1,18 +1,31 @@
+"""
+Author: Cancan Huang
+Date: 2020-09-22 20:35:59
+"""
 # Uses python3
 import sys
 from collections import namedtuple
 
-Segment = namedtuple('Segment', 'start end')
+Segment = namedtuple("Segment", "start end")
+
 
 def optimal_points(segments):
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    # write your code here
+    segments = sorted(segments, key=lambda s: s[1])
+    while len(segments) > 0:
+        min_right = segments[0].end
+        points.append(min_right)
+        i = 0
+        while i < len(segments):
+            if segments[i].start <= min_right and min_right <= segments[i].end:
+                del segments[i]
+            else:
+                i += 1
     return points
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
