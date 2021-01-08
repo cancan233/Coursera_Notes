c4ba6871c2694218d823eeca8de053128b2d6872
cancan233 <cancan_huang@brown.edu>
2021-01-07 23:13:08 -0500

    update week 6

--- Algorithmic Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold.py
+++ Algorithmic Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold.py
@@ -1,15 +1,39 @@
 # Uses python3
 import sys
 
+
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    # result = 0
+    # for x in w:
+    #     if result + x <= W:
+    #         result = result + x
+    # return result
+    # w = [0] + w
+    # weight = [[0 for j in range(len(w))] for i in range(W + 1)]
+    # for i in range(1, len(w)):
+    #     for curr_W in range(1, W + 1):
+    #         prev = weight[curr_W][i - 1]
+    #         Val = weight[W - w[i]][i - 1] + w[i]
+    #         if Val < curr_W:
+    #             weight[curr_W][i] = max(Val, prev)
+    #         else:
+    #             weight[curr_W][i] = prev
+    # return weight[-1][-1]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
+    value = [[0 for j in range(len(w) + 1)] for i in range(W + 1)]
+    for i in range(1, W + 1):
+        for j in range(1, len(w) + 1):
+            value[i][j] = value[i][j - 1]
+            if w[j - 1] <= i:
+                temp = value[i - w[j - 1]][j - 1] + w[j - 1]
+                if temp > value[i][j]:
+                    value[i][j] = temp
+    return value[W][len(w)]
+
+
+if __name__ == "__main__":
+    # input = sys.stdin.read()
+    input = "20 4 5 7 12 18"
     W, n, *w = list(map(int, input.split()))
     print(optimal_weight(W, w))
--- Algorithmic Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs.py
+++ Algorithmic Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs.py
@@ -2,19 +2,43 @@
 import sys
 import itertools
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
+def partition3(A):
+    # for c in itertools.product(range(3), repeat=len(A)):
+    #     sums = [None] * 3
+    #     for i in range(3):
+    #         sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+
+    #     if sums[0] == sums[1] and sums[1] == sums[2]:
+    #         return 1
+
+    # return 0
+
+    if len(A) < 3:
+        return 0
+    elif sum(A) % 3 != 0:
+        return 0
+    else:
+        W = sum(A) // 3
+        count = 0
+        value = [[0 for j in range(len(A) + 1)] for i in range(W + 1)]
+        for i in range(1, W + 1):
+            for j in range(1, len(A) + 1):
+                value[i][j] = value[i][j - 1]
+                if A[j - 1] <= i:
+                    temp = value[i - A[j - 1]][j - 1] + A[j - 1]
+                    if temp > value[i][j]:
+                        value[i][j] = temp
+                if value[i][j] == W:
+                    count += 1
+        if count < 3:
+            return 0
+        else:
             return 1
 
-    return 0
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
+if __name__ == "__main__":
+    # input = sys.stdin.read()
+    input = "4 4 1 3 2"
     n, *A = list(map(int, input.split()))
     print(partition3(A))
-
